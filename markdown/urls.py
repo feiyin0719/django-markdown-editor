376804ee35acb99839a759bdef9fe8b6af2ca66a
feiyin0719 <582816317@qq.com>
2018-01-08 18:25:48 +0800

    change url to fit django>=1.9

--- markdown/urls.py
+++ markdown/urls.py
@@ -1,7 +1,12 @@
-from django.conf.urls import patterns, url
-
+from django import VERSION
 from markdown.views import upload_image
-
-urlpatterns = patterns('',
-    url(r'^uploadimage/$', upload_image),
-)
+if  VERSION<(1,9):
+    from django.conf.urls import patterns, url
+    urlpatterns = patterns('',
+        url(r'^uploadimage/$', upload_image),
+    )
+else:
+    from django.conf.urls import  url
+    urlpatterns=[
+        url(r'^uploadimage/$', upload_image),
+    ]
